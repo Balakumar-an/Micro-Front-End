8ab125a1d69a2b95796fb114a172beb0e5dc72f7
Balakumar A N <ban0001@safeway.com>
2024-12-22 00:11:04 +0530

    fixed minor import issue in dashboard

--- packages/dashboard/config/webpack.prod.js
+++ packages/dashboard/config/webpack.prod.js
@@ -14,7 +14,7 @@ const prodConfig={
             name:"dashboard",
             filename:"remoteEntry.js",
             exposes:{
-                "./DashboardApp":"./src/bootstrap"
+                "./DashboardApp":"/src/bootstrap"
             },
             shared:packageJSON.dependencies
         })
